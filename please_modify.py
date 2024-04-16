d790b0b089f315b70434702634eadd08484e7074
s-y-u-n <114199496+s-y-u-n@users.noreply.github.com>
2024-04-16 23:04:01 +0900

    return_negativeの追加

--- please_modify.py
+++ please_modify.py
@@ -3,6 +3,11 @@ import numpy as np
 def square_input(x):
     return np.square(x)
 
+def return_negative(x):
+    return -x
+
 input = 3.5
-output = square_input(input)
-print(output)
+output_1 = square_input(input)
+output_2 = return_negative(input)
+print(output_1)
+print(output_2)
